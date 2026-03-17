7c761e0cd92cec52d6daf5ae2fba9d2e97b7a053
Alex Chernika <me@chernika.dev>
2026-03-17 19:32:13 +0100

    render: paint surface black with cairo

--- src/main.c
+++ src/main.c
@@ -7,9 +7,9 @@
 int main()
 {
   struct state *state = state_init();
-  wl_surface_attach(state->surface, create_buffer(state), 0, 0);
-  wl_surface_damage_buffer(state->surface, 0, 0, state->width, state->height);
-  wl_surface_commit(state->surface);
+  /* wl_surface_attach(state->surface, create_buffer(state), 0, 0); */
+  /* wl_surface_damage_buffer(state->surface, 0, 0, state->width, state->height); */
+  /* wl_surface_commit(state->surface); */
 
   while (wl_display_dispatch(state->display) != -1)
     {
--- src/wayland.h
+++ src/wayland.h
@@ -4,6 +4,7 @@
 #include "state.h"
 
 void wayland_init_globals(struct state *state);
-struct wl_buffer * create_buffer(struct state *state);
+
+struct wl_buffer *create_buffer(struct state *state);
 
 #endif
