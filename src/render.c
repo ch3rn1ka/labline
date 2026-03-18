b2933890eec171c754ecff1d77a5bc8e020432c5
Alex Chernika <me@chernika.dev>
2026-03-18 11:15:38 +0100

    render: render a simple string
    Trying to get familiar with pangocairo. The program now displays
    a simple "Hello, world!" aligned to the right.

--- src/render.c
+++ src/render.c
@@ -17,4 +17,33 @@ render(void *buffer, struct state *state)
   cairo_t *ctx = cairo_create(surface);
   cairo_set_source_rgb(ctx, 0.0, 0.0, 0.0);
   cairo_paint_with_alpha(ctx, 1.0);
+
+  PangoLayout *layout = pango_cairo_create_layout(ctx);
+  pango_layout_set_text(layout, state->text, -1);
+
+  PangoFontDescription *desc = pango_font_description_from_string("Monospace");
+  pango_layout_set_font_description(layout, desc);
+  pango_font_description_free(desc);
+
+  pango_layout_set_width(layout, state->width * PANGO_SCALE);
+  pango_layout_set_alignment(layout, PANGO_ALIGN_RIGHT);
+
+  int text_width, text_height;
+  pango_layout_get_pixel_size(layout, &text_width, &text_height);
+
+  /* used to center the text vertically */
+  double y_offset = (state->height - text_height) / 2.0;
+
+  cairo_set_source_rgb(ctx, 1.0, 1.0, 1.0);
+  /*
+   * Even though we start drawing from (0, y_offset), the text gets shifted
+   * by (state->width - text_width)px to the right since we aligned it earlier
+   * with `pango_layout_set_alignment()`.
+   */
+  cairo_move_to(ctx, 0, y_offset);
+  pango_cairo_show_layout(ctx, layout);
+
+  g_object_unref(layout);
+  cairo_destroy(ctx);
+  cairo_surface_destroy(surface);
 }
--- src/state.c
+++ src/state.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "wayland.h"
 #include "state.h"
@@ -9,10 +10,22 @@ state_init()
   struct state *state = malloc(sizeof(struct state));
   wl_list_init(&state->workspaces);
 
-  /* TODO: NOT hardcode these values */
-  state->width = 1920;
+  /*
+   * The width of the surface is left blank until `layer_surface_listener`
+   * catches a configure event from the compositor (it knows the horizontal
+   * dimension of the surface because we anchor it to the sides of the screen).
+   * As for the height, however, it's important to define it here because the
+   * compositor can't deduce it by itself, and we later communicate it in the
+   * `zwlr_layer_surface_v1_set_size()` call.
+   */
+  state->width = 0;
+  state->stride = 0;
+
+  /* TODO: get height from the fontsize */
   state->height = 26;
-  state->stride = 1920*4;
+
+  /* TODO: get text from stdin */
+  strcpy(state->text, "Hello, world!");
 
   wayland_init_globals(state);
 
