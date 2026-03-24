71ecb1d7cb068e1bf4d9cab8d660a7a2208ba9f7
Alex Chernika <me@chernika.dev>
2026-03-24 18:30:55 +0100

    all: format source following labwc coding style

--- src/wayland.c
+++ src/wayland.c
@@ -14,79 +14,77 @@
 #include "ext-workspace-v1-client-protocol.h"
 #include "wlr-layer-shell-unstable-v1-client-protocol.h"
 
-struct workspace
-{
-  struct ext_workspace_handle_v1 *handle;
-  char *name;
-  struct wl_list node;
+struct workspace {
+	struct ext_workspace_handle_v1 *handle;
+	char *name;
+	struct wl_list node;
 };
 
 static void
 registry_global(void *data, struct wl_registry *wl_registry,
-                uint32_t name, const char *iface,
-                uint32_t server_iface_version)
+		uint32_t name, const char *iface, uint32_t server_iface_version)
 {
-  struct state *state = data;
+	struct state *state = data;
 
-  const struct wl_interface *i = NULL; /* shorthand for wl_*_interface */
-  uint32_t bind_version, library_iface_version;
-  if (strcmp(iface, wl_compositor_interface.name) == 0)
-    {
-      i = &wl_compositor_interface;
-      library_iface_version = i->version;
-      bind_version = MIN(server_iface_version, library_iface_version);
-      state->compositor = wl_registry_bind(wl_registry, name, i, bind_version);
-    }
-  else if (strcmp(iface, wl_shm_interface.name) == 0)
-    {
-      i = &wl_shm_interface;
-      library_iface_version = i->version;
-      bind_version = MIN(server_iface_version, library_iface_version);
-      state->shm = wl_registry_bind(wl_registry, name, i, bind_version);
-    }
-  else if (strcmp(iface, zwlr_layer_shell_v1_interface.name) == 0)
-    {
-      i = &zwlr_layer_shell_v1_interface;
-      library_iface_version = i->version;
-      bind_version = MIN(server_iface_version, library_iface_version);
-      state->layer_shell = wl_registry_bind(wl_registry, name, i, bind_version);
-    }
-  else if (strcmp(iface, ext_workspace_manager_v1_interface.name) == 0)
-    {
-      i = &ext_workspace_manager_v1_interface;
-      library_iface_version = i->version;
-      bind_version = MIN(server_iface_version, library_iface_version);
-      state->workspace_manager = wl_registry_bind(wl_registry, name, i,
-                                                  bind_version);
-    }
+	const struct wl_interface *i = NULL; /* shorthand for wl_*_interface */
+	uint32_t bind_version, library_iface_version;
+	if (strcmp(iface, wl_compositor_interface.name) == 0)
+	{
+		i = &wl_compositor_interface;
+		library_iface_version = i->version;
+		bind_version = MIN(server_iface_version, library_iface_version);
+		state->compositor =
+			wl_registry_bind(wl_registry, name, i, bind_version);
+	} else if (strcmp(iface, wl_shm_interface.name) == 0) {
+		i = &wl_shm_interface;
+		library_iface_version = i->version;
+		bind_version = MIN(server_iface_version, library_iface_version);
+		state->shm =
+			wl_registry_bind(wl_registry, name, i, bind_version);
+	} else if (strcmp(iface, zwlr_layer_shell_v1_interface.name) == 0) {
+		i = &zwlr_layer_shell_v1_interface;
+		library_iface_version = i->version;
+		bind_version = MIN(server_iface_version, library_iface_version);
+		state->layer_shell =
+			wl_registry_bind(wl_registry, name, i, bind_version);
+	} else if (strcmp(iface,
+			ext_workspace_manager_v1_interface.name) == 0) {
+		i = &ext_workspace_manager_v1_interface;
+		library_iface_version = i->version;
+		bind_version = MIN(server_iface_version, library_iface_version);
+		state->workspace_manager =
+			wl_registry_bind(wl_registry, name, i, bind_version);
+	}
 }
 
 static void
 registry_global_remove() {}
 
 static const struct wl_registry_listener registry_listener = {
-  .global        = &registry_global,
-  .global_remove = &registry_global_remove
+	.global = registry_global,
+	.global_remove = registry_global_remove
 };
 
 static void
 workspace_handle_name(void *data, struct ext_workspace_handle_v1 *handle,
-                      const char *name)
+		const char *name)
 {
-  struct workspace *workspace = data;
-  if (workspace->name) free(workspace->name);
-  workspace->name = strdup(name);
+	struct workspace *workspace = data;
+	if (workspace->name) {
+		free(workspace->name);
+	}
+	workspace->name = strdup(name);
 }
 
 static void
 workspace_handle_state(void *data,
-                       struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
-                       uint32_t state)
+		struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
+		uint32_t state)
 {
-  struct workspace *workspace = data;
+	struct workspace *workspace = data;
 
-  /* TODO: mark workspace with `state`=1 as current */
-  printf("State changed for %s: %d\n", workspace->name, state);
+	/* TODO: mark workspace with state=1 as current */
+	printf("State changed for %s: %d\n", workspace->name, state);
 }
 
 static void
@@ -101,31 +99,30 @@ workspace_handle_capabilities() {}
 static void
 workspace_handle_removed()
 {
-  /* TODO: implement remove logic */
+	/* TODO: implement remove logic */
 }
 
 static const struct ext_workspace_handle_v1_listener
 workspace_handle_listener = {
-  .id           = &workspace_handle_id,
-  .name         = &workspace_handle_name,
-  .coordinates  = &workspace_handle_coordinates,
-  .state        = &workspace_handle_state,
-  .capabilities = &workspace_handle_capabilities,
-  .removed      = &workspace_handle_removed,
+	.id = workspace_handle_id,
+	.name = workspace_handle_name,
+	.coordinates = workspace_handle_coordinates,
+	.state = workspace_handle_state,
+	.capabilities = workspace_handle_capabilities,
+	.removed = workspace_handle_removed
 };
 
 static void
-workspace_manager_workspace(void *data,
-                            struct ext_workspace_manager_v1 *mgr,
-                            struct ext_workspace_handle_v1 *handle)
+workspace_manager_workspace(void *data, struct ext_workspace_manager_v1 *mgr,
+		struct ext_workspace_handle_v1 *handle)
 {
-  struct state *state = data;
-  struct workspace *new_workspace = calloc(1, sizeof(struct workspace));
-  new_workspace->handle = handle;
+	struct state *state = data;
+	struct workspace *new_workspace = calloc(1, sizeof(struct workspace));
+	new_workspace->handle = handle;
 
-  ext_workspace_handle_v1_add_listener(handle, &workspace_handle_listener,
-                                       new_workspace);
-  wl_list_insert(&state->workspaces, &new_workspace->node);
+	ext_workspace_handle_v1_add_listener(handle,
+		&workspace_handle_listener, new_workspace);
+	wl_list_insert(&state->workspaces, &new_workspace->node);
 }
 
 /*
@@ -143,95 +140,89 @@ workspace_manager_finished() {}
 
 static const struct ext_workspace_manager_v1_listener
 workspace_manager_listener = {
-  .workspace       = &workspace_manager_workspace,
-  .workspace_group = &workspace_manager_group,
-  .done            = &workspace_manager_done,
-  .finished        = &workspace_manager_finished
+	.workspace = workspace_manager_workspace,
+	.workspace_group = workspace_manager_group,
+	.done = workspace_manager_done,
+	.finished = workspace_manager_finished
 };
 
 /* Main buffer realloc logic here. */
 static void
-layer_surface_configure(void *data,
-                        struct zwlr_layer_surface_v1 *layer_surface,
-                        uint32_t serial, uint32_t width,
-                        uint32_t height)
+layer_surface_configure(void *data, struct zwlr_layer_surface_v1 *layer_surface,
+		uint32_t serial, uint32_t width, uint32_t height)
 {
-  struct state *state = data;
+	struct state *state = data;
 
-  if (width != state->width || height != state->height)
-    {
-      state->width = width;
-      state->stride = width * 4;
-      state->height = height;
+	if (width != state->width || height != state->height) {
+		state->width = width;
+		state->stride = width * 4;
+		state->height = height;
 
-      state->buffers[0]->stale = true;
-      state->buffers[1]->stale = true;
-    }
-  zwlr_layer_surface_v1_ack_configure(layer_surface, serial);
+		state->buffers[0]->stale = true;
+		state->buffers[1]->stale = true;
+	}
+	zwlr_layer_surface_v1_ack_configure(layer_surface, serial);
 
-  render(state);
+	render(state);
 }
 
 static void
-layer_surface_closed(void *data,
-                     struct zwlr_layer_surface_v1 *layer_surface)
+layer_surface_closed(void *data, struct zwlr_layer_surface_v1 *layer_surface)
 {
   /* TODO: write close logic */
 }
 
 static const struct zwlr_layer_surface_v1_listener layer_surface_listener = {
-  .configure = &layer_surface_configure,
-  .closed    = &layer_surface_closed
+	.configure = layer_surface_configure,
+	.closed = layer_surface_closed
 };
 
 
 void
-init_wayland_globals(struct state *state)
+wayland_init(struct state *state)
 {
-  state->display = wl_display_connect(NULL);
-  if (!state->display)
-    {
-      fprintf(stderr, "Failed to connect to display.\n");
-      exit(EXIT_FAILURE);
-    }
+	state->display = wl_display_connect(NULL);
+	if (!state->display)
+	{
+		fprintf(stderr, "Failed to connect to display.\n");
+		exit(EXIT_FAILURE);
+	}
 
-  state->registry = wl_display_get_registry(state->display);
-  if (!state->registry)
-    {
-      fprintf(stderr, "Failed to get registry.\n");
-      exit(EXIT_FAILURE);
-    }
-  wl_registry_add_listener(state->registry, &registry_listener, state);
-  wl_display_roundtrip(state->display);
+	state->registry = wl_display_get_registry(state->display);
+	if (!state->registry)
+	{
+		fprintf(stderr, "Failed to get registry.\n");
+		exit(EXIT_FAILURE);
+	}
+	wl_registry_add_listener(state->registry, &registry_listener, state);
+	wl_display_roundtrip(state->display);
 
-  if (!state->workspace_manager)
-    {
-      fprintf(stderr, "Workspace manager not supported by the compositor.\n");
-      exit(EXIT_FAILURE);
-    }
-  ext_workspace_manager_v1_add_listener(state->workspace_manager,
-                                        &workspace_manager_listener,
-                                        state);
-  wl_display_roundtrip(state->display);
+	if (!state->workspace_manager)
+	{
+		fprintf(stderr,
+			"Workspace manager not supported by the compositor.\n");
+		exit(EXIT_FAILURE);
+	}
+	ext_workspace_manager_v1_add_listener(state->workspace_manager,
+		&workspace_manager_listener, state);
+	wl_display_roundtrip(state->display);
 
-  state->surface = wl_compositor_create_surface(state->compositor);
-  state->layer_surface =
-    zwlr_layer_shell_v1_get_layer_surface(state->layer_shell,
-                                          state->surface,
-                                          NULL, // wl_output
-                                          ZWLR_LAYER_SHELL_V1_LAYER_TOP,
-                                          "labline");
-  zwlr_layer_surface_v1_set_size(state->layer_surface, 0, state->height);
-  zwlr_layer_surface_v1_set_anchor(state->layer_surface,
-                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
-                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT |
-                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
-  zwlr_layer_surface_v1_set_exclusive_zone(state->layer_surface,
-                                           state->height);
-  zwlr_layer_surface_v1_add_listener(state->layer_surface,
-                                     &layer_surface_listener, state);
+	state->surface = wl_compositor_create_surface(state->compositor);
+	state->layer_surface =
+		zwlr_layer_shell_v1_get_layer_surface(state->layer_shell,
+			state->surface, NULL, ZWLR_LAYER_SHELL_V1_LAYER_TOP,
+			"labline");
+	zwlr_layer_surface_v1_set_size(state->layer_surface, 0, state->height);
+	zwlr_layer_surface_v1_set_anchor(state->layer_surface,
+		ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM
+		| ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT
+		| ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
+	zwlr_layer_surface_v1_set_exclusive_zone(state->layer_surface,
+		state->height);
+	zwlr_layer_surface_v1_add_listener(state->layer_surface,
+		&layer_surface_listener, state);
 
-  wl_surface_commit(state->surface);
-  wl_display_flush(state->display);
-  wl_display_roundtrip(state->display);
+	wl_surface_commit(state->surface);
+	wl_display_flush(state->display);
+	wl_display_roundtrip(state->display);
 }
