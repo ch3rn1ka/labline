00ffd08cc9f6832a14be59cc2408ef34516f0a79
Alex Chernika <me@chernika.dev>
2026-03-24 18:30:55 +0100

    style: format source following labwc coding style

--- src/main.c
+++ src/main.c
@@ -8,52 +8,44 @@
 #include "shm.h"
 #include "wayland.h"
 
-int main()
+int
+main(int argc, char **argv)
 {
-  struct state *state = init_state();
+	struct state *state = state_init();
 
-  struct pollfd fds[2];
-  fds[0].fd = STDIN_FILENO;
-  fds[0].events = POLLIN;
-  fds[1].fd = wl_display_get_fd(state->display);
-  fds[1].events = POLLIN;
+	struct pollfd fds[2];
+	fds[0].fd = STDIN_FILENO;
+	fds[0].events = POLLIN;
+	fds[1].fd = wl_display_get_fd(state->display);
+	fds[1].events = POLLIN;
 
-  while (true)
-    {
-      while (wl_display_prepare_read(state->display) != 0)
-        {
-          wl_display_dispatch_pending(state->display);
-        }
-      wl_display_flush(state->display);
+	while (true){
+		while (wl_display_prepare_read(state->display) != 0) {
+			wl_display_dispatch_pending(state->display);
+		}
+		wl_display_flush(state->display);
 
-      if (poll(fds, 2, -1) <= 0)
-        {
-          wl_display_cancel_read(state->display);
-          break;
-        }
+		if (poll(fds, 2, -1) <= 0) {
+			wl_display_cancel_read(state->display);
+			break;
+		}
 
-      if (fds[1].revents & POLLIN)
-        {
-          wl_display_read_events(state->display);
-        }
-      else
-        {
-          wl_display_cancel_read(state->display);
-        }
+		if (fds[1].revents & POLLIN) {
+			wl_display_read_events(state->display);
+		} else {
+			wl_display_cancel_read(state->display);
+		}
 
-      if (fds[0].revents & POLLIN)
-        {
-          if (fgets(state->text, BUFSIZ, stdin))
-            {
-              int length = strlen(state->text);
-              if (state->text[length - 1] == '\n')
-                {
-                  state->text[length - 1] = '\0';
-                }
-              render(state);
-            }
-        }
-    }
+		if (fds[0].revents & POLLIN) {
+			if (fgets(state->text, BUFSIZ, stdin)) {
+				int length = strlen(state->text);
+				if (state->text[length - 1] == '\n') {
+					state->text[length - 1] = '\0';
+				}
+				render(state);
+			}
+		}
+	}
 
-  return 0;
+	return 0;
 }
