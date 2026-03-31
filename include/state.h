599ed0ad1308d51869235b64006069b70d2cfbf8
Alex Chernika <me@chernika.dev>
2026-03-31 17:12:09 +0200

    feat: tie panel height to font size

--- include/state.h
+++ include/state.h
@@ -6,25 +6,26 @@
 
 struct state
 {
-  struct wl_display *display;
-  struct wl_registry *registry;
-  struct wl_compositor *compositor;
-  struct wl_shm *shm;
-  struct wl_surface *surface;
-  struct zwlr_layer_shell_v1 *layer_shell;
-  struct zwlr_layer_surface_v1 *layer_surface;
-  struct ext_workspace_manager_v1 *workspace_manager;
+	struct wl_display *display;
+	struct wl_registry *registry;
+	struct wl_compositor *compositor;
+	struct wl_shm *shm;
+	struct wl_surface *surface;
+	struct zwlr_layer_shell_v1 *layer_shell;
+	struct zwlr_layer_surface_v1 *layer_surface;
+	struct ext_workspace_manager_v1 *workspace_manager;
 
-  struct wl_list workspaces;
+	struct wl_list workspaces;
 
-  /* Use double buffering to avoid reallocating on each configure event */
-  struct buffer_context *buffers[2];
-  char statusline[BUFSIZ];
+	/* Use double buffering to avoid reallocating on each configure event */
+	struct buffer_context *buffers[2];
+	char statusline[BUFSIZ];
 
-  uint32_t width, height;
-  int stride;
+	char *font;
+	int font_height;
 
-  int anchor;
+	uint32_t width, height, stride;
+	int anchor;
 };
 
 struct state *state_init();
--- src/state.c
+++ src/state.c
@@ -20,6 +20,22 @@ buffers_init(struct state *state)
 	}
 }
 
+static int
+get_font_height(const char *fontname)
+{
+	PangoFontMap *fontmap = pango_cairo_font_map_get_default();
+	PangoContext *context = pango_font_map_create_context(fontmap);
+	PangoFontDescription *desc = pango_font_description_from_string(fontname);
+	PangoFont *font = pango_font_map_load_font(fontmap, context, desc);
+	PangoFontMetrics *metrics = pango_font_get_metrics(font, NULL);
+	int height = pango_font_metrics_get_height(metrics) / PANGO_SCALE;
+	pango_font_metrics_unref(metrics);
+	g_object_unref(font);
+	pango_font_description_free(desc);
+	g_object_unref(context);
+	return height;
+}
+
 struct state *
 state_init()
 {
@@ -32,8 +48,9 @@ state_init()
 	state->width = 0;
 	state->stride = 0;
 
-	/* TODO: get height from the fontsize */
-	state->height = 26;
+	state->font = "Monospace 10";
+	state->font_height = get_font_height(state->font);
+	state->height = state->font_height + 6;
 
 	wl_list_init(&state->workspaces);
 	buffers_init(state);
