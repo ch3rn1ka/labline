79694f5eccbf55e69c8aadedc8cee139ec40988e
Alex Chernika <me@chernika.dev>
2026-06-24 07:52:37 +0200

    refactor: clean up buffer.c
    - Rename the `map` and `map_size` fields of the `buffer_context` struct
    to `buf` and `buf_size` respectively
    - Add error handling
    - Stop storing file descriptors after mapping shared memory

--- src/buffer.c
+++ src/buffer.c
@@ -9,6 +9,7 @@
 #include "buffer.h"
 #include "shm.h"
 #include "state.h"
+#include "util.h"
 
 /*
  * Allocate two `buffer_context` structs and mark them as stale so that their
@@ -19,7 +20,6 @@ buffers_init(struct labline_state *state)
 {
 	for (int i = 0; i < 2; i++) {
 		state->buffers[i] = calloc(1, sizeof(struct buffer_context));
-		state->buffers[i]->fd = -1;
 		state->buffers[i]->stale = true;
 	}
 }
@@ -47,37 +47,35 @@ buffer_realloc(struct buffer_context *buf_ctx, struct labline_state *state)
 	if (buf_ctx->wl_buffer) {
 		wl_buffer_destroy(buf_ctx->wl_buffer);
 	}
-	if (buf_ctx->map) {
-		munmap(buf_ctx->map, buf_ctx->map_size);
+	if (buf_ctx->buf) {
+		munmap(buf_ctx->buf, buf_ctx->buf_size);
 	}
-	if (buf_ctx->fd >= 0) {
-		close(buf_ctx->fd);
-	}
-
-	buf_ctx->map_size = state->height * state->stride;
-	int fd = create_shm_file(buf_ctx->map_size);
-	/* TODO: handle errors */
-
-	void *map = mmap(NULL, buf_ctx->map_size,
-		PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	/* TODO: handle errors */
-	buf_ctx->map = map;
 
 	/* Buffer */
+	buf_ctx->buf_size = state->height * state->stride;
+	int fd = create_shm_file(buf_ctx->buf_size);
+
+	void *buf = mmap(NULL, buf_ctx->buf_size,
+		PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (buf == MAP_FAILED) {
+		die("Failed to run mmap()");
+	}
+	buf_ctx->buf = buf;
+
 	struct wl_shm_pool *pool = wl_shm_create_pool(state->shm, fd,
-		buf_ctx->map_size);
+		buf_ctx->buf_size);
 	buf_ctx->wl_buffer = wl_shm_pool_create_buffer(pool, 0, state->width,
 		state->height, state->stride, WL_SHM_FORMAT_ARGB8888);
 	wl_shm_pool_destroy(pool);
+	close(fd);
 
 	/* Pango/cairo globals */
 	buf_ctx->cairo_surface =
-		cairo_image_surface_create_for_data(map, CAIRO_FORMAT_ARGB32,
+		cairo_image_surface_create_for_data(buf, CAIRO_FORMAT_ARGB32,
 			state->width, state->height, state->stride);
 	buf_ctx->cairo_ctx = cairo_create(buf_ctx->cairo_surface);
 	buf_ctx->pango_layout = pango_cairo_create_layout(buf_ctx->cairo_ctx);
 
-	/* TODO: custom fonts */
 	buf_ctx->pango_font_desc =
 		pango_font_description_from_string(state->font);
 
@@ -85,7 +83,6 @@ buffer_realloc(struct buffer_context *buf_ctx, struct labline_state *state)
 		buf_ctx->pango_font_desc);
 	pango_layout_set_width(buf_ctx->pango_layout, state->width * PANGO_SCALE);
 
-	buf_ctx->fd = fd;
 	buf_ctx->stale = false;
 	buf_ctx->busy = false;
 }
